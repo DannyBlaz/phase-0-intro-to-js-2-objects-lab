cc77b2c90b2311900cdf4f2c908ce28118aaafdb
Daniel <dannyblaze469@gmail.com>
2021-07-01 17:03:19 -0400

    commit tree 8f6d919d75250a95a8efc8fcb0436a047911cc3f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const employee = {
+    name: don,
+    streetAddress:"23 zebra dr"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    // const newEmployee = {...employee}
+    // newEmployee[key]=value
+    return Object.assign({}, employee, { [key]: value });
+    return newEmployee
+}
